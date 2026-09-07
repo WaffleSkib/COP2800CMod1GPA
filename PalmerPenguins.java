5e22ba0d683288715fc3ad7aa48677a9662fd2c9
WaffleSkib <wolfieeheart@gmail.com>
2026-09-06 23:34:39 -0400

    Committed first assignment source code

---
+++ PalmerPenguins.java
@@ -0,0 +1,11 @@
+// PalmerPenguins.java
+// Saryn Vasallo
+// September 6, 2026
+// Introduce Palmer Penguins dataset
+
+public class PalmerPenguins {
+    public static void main(String[] args) {
+
+
+    }
+}
